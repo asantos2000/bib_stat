b520a9fc77893839af8c378d48f90e06bc0a793f
Anderson Santos <adsantos@gmail.com>
2024-01-03 22:03:24 -0300

    convert bib to csv

---
+++ convert_bib.py
@@ -0,0 +1,66 @@
+import logging
+import sys
+import csv
+import os
+import argparse
+from pybtex.database.input import bibtex
+
+parser = argparse.ArgumentParser()
+parser.add_argument('path', help='Define the path of the BibTex file to convert.')
+parser.add_argument('-dl', '--delimiter', help='Specify the delimiter for importing the CSV.', default=',')
+parser.add_argument('-out', help='Define the path of the CSV the data gets outputted to.', required=False)
+parser.add_argument('-rf', '--rm-fields', nargs='*', help='Specify fields to remove from the output file.', required=False)
+args = parser.parse_args()
+
+parser = bibtex.Parser()
+
+bib_data = parser.parse_file(args.path)
+
+fields_to_remove = args.rm_fields if args.rm_fields else []
+
+output_fn = args.out + ('' if args.out.endswith('.csv') else '.csv') if args.out else args.path.replace('bib', 'csv')
+
+csv_fields = []
+csv_data = []
+if os.path.exists(output_fn):
+	with open(output_fn) as csvfile:
+		csv_data.extend(list(csv.DictReader(csvfile, delimiter=args.delimiter)))
+		csv_fields.extend([item for sublist in [row.keys() for row in csv_data] for item in sublist])
+
+try:
+	csv_ids = [value['id'] for value in csv_data]
+except KeyError:
+	logging.error('You may have specified the wrong delimiter ("{}") to import the CSV file.'.format(args.delimiter))
+	sys.exit(1)
+
+fields = list(filter(lambda x: x not in fields_to_remove,
+					 set([item for sublist in
+						  [value.fields.keys() for value in bib_data.entries.values()]
+						  for item in sublist] + csv_fields + ['id', 'type', 'authors'])))
+
+rows = []
+for key, value in bib_data.entries.items():
+	if value.key not in csv_ids:
+		row = {}
+		try:
+			for field in fields:
+				if field == 'id':
+					row[field] = value.key
+				elif field == 'type':
+					row[field] = value.type
+				elif field == 'authors':
+					row[field] = ', '.join([' '.join(list(filter(None, [' '.join(person.get_part(part)) for part in ['first', 'middle', 'last']])))
+											for person in value.persons['author']])
+				else:
+					row[field] = value.fields[field] if field in value.fields.keys() else None
+			
+			rows.append(row)
+		except KeyError as e:
+			print(f'Error: {e}')
+
+rows.extend([{field: value[field] if field in value.keys() else None for field in fields} for value in csv_data])
+
+with open(output_fn, 'w') as output_file:
+	dict_writer = csv.DictWriter(output_file, rows[0].keys())
+	dict_writer.writeheader()
+	dict_writer.writerows(rows)
